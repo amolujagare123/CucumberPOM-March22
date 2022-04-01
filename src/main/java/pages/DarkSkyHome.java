0bf3ba5776c8f27c0216a795224af3df68ac47da
amolu <amolujagare@gmail.com>
2022-04-02 01:58:42 +0530

    Cucumber project

--- src/main/java/pages/DarkSkyHome.java
+++ src/main/java/pages/DarkSkyHome.java
@@ -2,10 +2,32 @@ package pages;
 
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
+
 public class DarkSkyHome extends Base{
 
     By currentTempText = By.xpath("//span[@class='summary swap']");
     By timelineTempText = By.xpath("//span[@class='first']//span");
+    By timeListRaw = By.xpath("//span[@class='hour']/span");
+
+    public ArrayList<Integer> getTimelist()
+    {
+        ArrayList<String> timelistStr = getElementTextList(timeListRaw);
+        // [9pm,11pm,3am,......]
+        System.out.println(timelistStr);
+        ArrayList<Integer> timeList = new ArrayList<>();
+
+        for(int i=0;i<timelistStr.size();i++)
+        {
+            String temp = timelistStr.get(i); // 9pm / 11pm / 3am
+            int l = temp.length();
+            String timeStr = temp.substring(0, l - 2); // 9 / 11 / 3
+            int time = Integer.parseInt(timeStr);
+            timeList.add(time);
+        }
+        System.out.println(timeList);
+        return timeList;
+    }
 
     public int getTimelineTemp()
     {
--- src/main/java/stepdefinition/DarksySD.java
+++ src/main/java/stepdefinition/DarksySD.java
@@ -5,6 +5,11 @@ import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.DarkSkyHome;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static stepdefinition.SharedSD.getDriver;
+
 public class DarksySD {
 
     DarkSkyHome darkSkyHome = new DarkSkyHome();
@@ -13,6 +18,10 @@ public class DarksySD {
     public void iAmOnDarkskyHomePage() {
 
         // check this is the darksky home page or not
+
+        Assert.assertEquals("this is not a darksky home page",
+                "Dark Sky - Sansad Marg, New Delhi, Delhi",
+                getDriver().getTitle());
     }
 
     @Then("I verify current temp is equal to Temperature from Daily Timeline")
@@ -26,4 +35,35 @@ public class DarksySD {
         Assert.assertEquals("temperatures are different",expected,actual);
 
     }
+
+    @Then("I verify timeline is displayed with two hours incremented")
+    public void iVerifyTimelineIsDisplayedWithTwoHoursIncremented() {
+
+        ArrayList<Integer> timeList = darkSkyHome.getTimelist(); // 11-> last index - 10
+        ArrayList<Integer> timeDiffList = new ArrayList<>(); // 10
+
+        for(int i=0;i<timeList.size()-1;i++) // iterate 11 times (0-10)
+        {
+            int time1 = timeList.get(i); // 10
+            int time2 = timeList.get(i+1); // 11
+            int timeDiff = 0;
+
+            if(time2>time1)
+                timeDiff = time2 - time1;
+            if(time2<time1)
+                timeDiff = (time2+12) - time1;
+
+            timeDiffList.add(timeDiff);
+        }
+
+        System.out.println(timeDiffList);
+        int size = timeDiffList.size();
+        int frequency = Collections.frequency(timeDiffList,2);
+
+        boolean result = (size == frequency);
+
+        Assert.assertTrue("all the differences are not 2",result);
+    }
+
+
 }
