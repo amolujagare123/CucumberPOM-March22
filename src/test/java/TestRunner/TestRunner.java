67425287cb76496c6d6490601ade6465cf1a08c8
amolu <amolujagare@gmail.com>
2022-04-27 21:34:13 +0530

    Cucumber project

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -5,8 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Features"
-        ,glue = "stepdefinition"
-,tags = "@PriceList")
+@CucumberOptions(features = "Features/Darksky.feature"
+        ,glue = "stepdefinition",
+        plugin = {"pretty","html:target/cucumber.html"}
+)
 public class TestRunner {
 }
